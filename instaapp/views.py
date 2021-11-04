b687d243d92cbd116bef8155565948fca377aa44
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-04 22:20:26 +0300

    jAdd functions to add and delete comment

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,11 +1,13 @@
 from django.http import request
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
-from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from .models import Profile, Post, Comment
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm, CommentForm, PostForm
 from django.contrib import messages
 from django .contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
 
 # Create your views here.
 
@@ -34,7 +36,7 @@ def loginPage(request):
             return redirect('auth')
             
         else:
-            messages.info(request,'Username or password is inorrect')
+            messages.info(request,'Username or password is incorrect')
             
 
     context = {}
@@ -84,3 +86,70 @@ def profile(request):
     }
 
     return render(request, 'accounts/profile.html', context)
+
+@login_required(login_url='login')
+def create_post(request):
+    current_user = request.user
+    if request.method == "POST":
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid:
+            post = form.save(commit= False)
+            post.author = current_user
+            post.save()
+        return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request,'create_post.html',{'form':form})
+
+
+
+    
+
+def add_comment(request,pk):
+    post = Post.objects.get(pk = pk)
+    form = CommentForm(request.POST,instance=post)
+    if request.method == "POST":
+        if form.is_valid():
+            name = request.user.username
+            comment_body = form.cleaned_data['comment_body']
+            context = Comment(post=post,name =name,comment_body =comment_body,date_added=datetime.now())
+
+            context.save()
+            return redirect('post_list')
+        else:
+            print('form is invalid')
+
+    else:
+        form = CommentForm
+
+    context = {
+        'form':form
+    }
+    return render(request,'tag.html',context)
+
+
+
+def delete_comment(request,pk):
+    comment =Comment.objects.filter(post = pk).last()
+    comment.delete()
+    return redirect('post_list')
+
+
+
+
+def search_results(request):
+    if 'author' in request.GET and request.GET["author"]:
+        search_term = request.GET.get("author")
+        searched_articles = Post.search_category(search_term)
+        message = f"{search_term}"
+        return render(request, 'search.html',{"message":message,"categories": searched_articles})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
+
+def index(request):
+    current_user = request.GET.get('user')
+    logged_in_user = request.user.username
+    return render(request,'index.html',{'current_user':current_user})
