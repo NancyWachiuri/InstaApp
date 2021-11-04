3c8c9b03579ef98bfdde4fb6d253cfea98870ed3
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-04 22:10:22 +0300

    Migrate Like and Comment classed to database

--- instaapp/models.py
+++ instaapp/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     image = models.ImageField(upload_to ='profile_pics')
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
 
 class Post(models.Model):
@@ -27,3 +30,29 @@ class Post(models.Model):
     def search_category(cls,search):
         searches = cls.objects.filter(author__username__icontains = search)
         return searches
+
+LIKE_CHOICES = (
+    ('Like','Like'),
+    ('Unlike','Unlike'),
+)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES,default='Like',max_length=10)
+
+    def __str__(self):
+        return str(self.post)
+
+      
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post',related_name='comments',on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    comment_body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.post.caption,self.name}'
+
