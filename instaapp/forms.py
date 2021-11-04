17dafcc44f727a931373b79929f5e1c521693319
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-04 22:19:00 +0300

    Add forms for commits and posts

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -3,7 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.forms import fields
-from .models import Profile
+from .models import Profile,Comment,Post
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit,Layout,Field
+
 
 class CreateUserForm(UserCreationForm):
     class Meta:
@@ -23,3 +26,26 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
+class PostForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('post', 'post',css_class = 'btn btn-success'))
+
+    class Meta:
+        model = Post
+        fields = [
+            'image',
+            'caption'
+        ]
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['comment_body',]
+        widgets = {
+            'comment_body':forms.Textarea(attrs={'class': 'form-control'}),
+        }
+            
