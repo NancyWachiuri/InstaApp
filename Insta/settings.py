669ff837cfe500f0d6ff4d19683cddfb31e589de
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-05 01:36:00 +0300

    Final Commit

--- Insta/settings.py
+++ Insta/settings.py
@@ -54,7 +54,8 @@ DATABASES['default'].update(db_from_env)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(),)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','herokuapp.com','127.0.0.1']
+
 
 
 
